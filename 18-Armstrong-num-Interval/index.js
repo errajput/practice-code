689cecbb330c3525d5d995a317ebaa39e884b55e
errajput <dishacsevira@gmail.com>
2024-08-22 10:56:07 +0530

    18-armstrong

---
+++ 18-Armstrong-num-Interval/index.js
@@ -0,0 +1,16 @@
+const minNum = 8;
+const maxNum = 500;
+console.log("Armstrong number");
+for (let i = minNum; i <= maxNum; i++) {
+  let numOfDigit = i.toString().length;
+  let sum = 0;
+  let temp = i;
+  while (temp > 0) {
+    let reminder = temp % 10;
+    sum += reminder ** numOfDigit;
+    temp = parseInt(temp / 10);
+  }
+  if (sum == i) {
+    console.log(i);
+  }
+}
