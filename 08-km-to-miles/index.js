0ac4362f41664928b3093ceb15aa56068fa49299
errajput <dishacsevira@gmail.com>
2024-09-03 16:27:27 +0530

    find value in browser

--- 08-km-to-miles/index.js
+++ 08-km-to-miles/index.js
@@ -1,5 +1,6 @@
-function kmToMiles(km, fact) {
-  const miles = fact * km;
+function kmToMiles(km) {
+  const miles = 0.62137 * km;
   console.log(`${km} kilometers is equal to the ${miles} miles`);
+  return miles;
 }
-kmToMiles(2.2, 7);
+kmToMiles(2.2);
